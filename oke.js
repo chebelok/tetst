f0ae4def913229b93d87d0bb6ced6e09538d3a46
cchebelok <lotokhinivan@gmail.com>
2022-01-02 22:28:24 +0200

    made a new code

--- oke.js
+++ oke.js
@@ -1,4 +1,18 @@
-'use strict'
-console.log('oke');
- let a = 4;
- console.log(a);
+let a = {fruit: 'apple', meat: 'beef'};
+
+let b = { vegetable: 'carrot', sweet: 'cake'};
+
+function createObj(obj1, obj2){
+    let food = {};
+    for (let i in obj1) {
+      food[i] = obj1[i];
+    }
+    for (let j in obj2) {
+      food[j] = obj2[j];
+    }
+    return food;
+};
+
+let res = createObj(a, b);
+
+console.log(res);
